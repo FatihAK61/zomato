689c28eb1c7d6815242d26bd5d479185a0e39f09
FatihAK61 <fatihak61@gmail.com>
2024-11-09 22:46:00 +0300

    Initial

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from '@features/auth/SplashScreen';
+import LoginScreen from '@features/auth/LoginScreen';
+import { navigationRef } from '@utils/NavigationUtils';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+    return (
+        <NavigationContainer ref={navigationRef}>
+            <Stack.Navigator initialRouteName="SplashScreen" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="SplashScreen" component={SplashScreen} />
+                <Stack.Screen options={{ animation: 'fade' }} name="LoginScreen" component={LoginScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default Navigation;
